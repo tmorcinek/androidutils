d38b29085d1a7db0e0c4833b1e5546155cf785c8
Tomasz Morcinek <tomasz@morcinek.co.uk>
2016-06-23 19:20:41 +0700

    Adding method 'readParcelableList' in ParcelableUtils.

--- src/main/java/com/morcinek/androidutils/utils/ParcelableUtils.kt
+++ src/main/java/com/morcinek/androidutils/utils/ParcelableUtils.kt
@@ -7,16 +7,21 @@ import java.util.*
 /**
  * Copyright 2016 Tomasz Morcinek. All rights reserved.
  */
-// Inline function to create Parcel Creator
-inline fun <reified T : Parcelable> createParcel(crossinline createFromParcel: (Parcel) -> T?): Parcelable.Creator<T> =
+
+inline fun <reified T : Parcelable> createCreator(crossinline createFromParcel: (Parcel) -> T?) =
         object : Parcelable.Creator<T> {
-            override fun createFromParcel(source: Parcel): T? = createFromParcel(source)
-            override fun newArray(size: Int): Array<out T?> = arrayOfNulls(size)
+            override fun createFromParcel(source: Parcel) = createFromParcel(source)
+            override fun newArray(size: Int) = arrayOfNulls<T>(size)
         }
 
-// custom readParcelable to avoid reflection
 fun <T : Parcelable> Parcel.readParcelable(creator: Parcelable.Creator<T>): T? {
     if (readString() != null) return creator.createFromParcel(this) else return null
+}
+
+fun <T : Parcelable> Parcel.readParcelableList(creator: Parcelable.Creator<T>): List<T> {
+    val mutableList = mutableListOf<T>()
+    readTypedList(mutableList, creator)
+    return mutableList
 }
 
 // Date
