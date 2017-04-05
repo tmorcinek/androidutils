ef102a15cf8e98dfd2ed4efede2576074af8e2df
Tomasz Morcinek <tomasz.morcinek@agoda.com>
2017-04-05 14:30:38 +0700

    Fixing PagerAdapter's crash when recreating Fragment.

--- src/main/java/com/morcinek/androidutils/pager/PagerAdapter.kt
+++ src/main/java/com/morcinek/androidutils/pager/PagerAdapter.kt
@@ -3,28 +3,20 @@ package com.morcinek.androidutils.pager
 import android.content.Context
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.app.FragmentStatePagerAdapter
 
 /**
  * Copyright 2016 Tomasz Morcinek. All rights reserved.
  */
-abstract class PagerAdapter(protected val context: Context, fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+abstract class PagerAdapter(protected val context: Context, fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager) {
 
     abstract val fragments: List<Fragment>
 
-    init {
-        fragmentManager.fragments?.clear()
-    }
-
-    override fun getItem(position: Int): Fragment {
-        return fragments[position]
-    }
+    override fun getItem(position: Int) = fragments[position]
 
     override fun getCount() = fragments.size
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return context.getString((getItem(position) as Page).title)
-    }
+    override fun getPageTitle(position: Int) = context.getString((getItem(position) as Page).title)
 
     interface Page {
         val title: Int
