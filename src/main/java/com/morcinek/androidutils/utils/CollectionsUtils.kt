fc8aa0e1483812fae697929ad20d23d5e735932c
tmorcinek <tomasz.morcinek@gmail.com>
2017-05-26 00:49:14 +0700

    Adding valueOrDefault extension method to map.

--- src/main/java/com/morcinek/androidutils/utils/CollectionsUtils.kt
+++ src/main/java/com/morcinek/androidutils/utils/CollectionsUtils.kt
@@ -14,3 +14,5 @@ fun <T> List<T>.head(numberOfElements: Int): List<T> {
     return subList(0, Math.min(size, numberOfElements))
 }
 
+fun <K, V> Map<K, V>.valueOrDefault(key: K, default: V) = if (contains(key)) get(key)!! else default
+
