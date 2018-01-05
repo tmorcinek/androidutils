2be13b0c0cc0be1d4e6b4c780996fbeda3379495
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2018-01-05 22:15:36 +0100

    Changing PagerAdapter from FragmentStatePagerAdapter to FragmentPagerAdapter.

--- src/main/java/com/morcinek/androidutils/pager/PagerAdapter.kt
+++ src/main/java/com/morcinek/androidutils/pager/PagerAdapter.kt
@@ -3,12 +3,12 @@ package com.morcinek.androidutils.pager
 import android.content.Context
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v4.app.FragmentPagerAdapter
 
 /**
  * Copyright 2016 Tomasz Morcinek. All rights reserved.
  */
-abstract class PagerAdapter(protected val context: Context, fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager) {
+abstract class PagerAdapter(protected val context: Context, fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
 
     abstract val fragments: List<Fragment>
 
